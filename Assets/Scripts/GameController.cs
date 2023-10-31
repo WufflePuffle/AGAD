7ef528b228584822ba696160504438cfa5d67c45
WufflePuffle <78995325+WufflePuffle@users.noreply.github.com>
2023-10-31 09:50:46 -0400

    Add files via upload

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,7 +7,7 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello World");
     }
 
     // Update is called once per frame
